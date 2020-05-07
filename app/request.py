91cdd38865c8661a675245f4aa15870b4a8b06af
derrickip34 <derrickip34@gmail.com>
2020-05-07 20:47:21 +0300

    create a function to search a movie

--- app/request.py
+++ app/request.py
@@ -4,12 +4,16 @@ from .models import Movie,Trailer
 api_key = None
 base_url = None
 trailers_url = None
+recommended_url = None
+search_url = None
 
 def configure_request(app):
-    global api_key,base_url,trailers_url
+    global api_key,base_url,trailers_url,recommended_url,search_url
     api_key = app.config['MOVIE_API_KEY']
     base_url = app.config['MOVIE_API_BASE_URL']
     trailers_url = app.config['TRAILERS_URL']
+    recommended_url = app.config['RECOMMENDED_MOVIES_URL']
+    search_url = app.config['SEARCH_URL']
 
 def get_movies(category):
 
@@ -84,3 +88,47 @@ def process_trailers(list_trailers):
         trailer_object = Trailer(id,key)
 
     return trailer_object
+
+def get_recommended_movies(id):
+    get_recommended_movies_url = recommended_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_recommended_movies_url) as url:
+        recommended_movies_data = url.read()
+        recommended_movies_response = json.loads(recommended_movies_data)
+
+        recommended_movies_results = None
+
+        if recommended_movies_response['results']:
+            recommended_movies_results_list = recommended_movies_response['results']
+            recommended_movies_results = process_recommended_movies(recommended_movies_results_list)
+
+    return recommended_movies_results
+
+def process_recommended_movies(recommended_movies_list):
+
+    recommended_movies_results = []
+    for recommended_movie in recommended_movies_list:
+        id = recommended_movie.get('id')
+        title = recommended_movie.get('original_title')
+        overview = recommended_movie.get('overview')
+        poster = recommended_movie.get('poster_path')
+
+        if poster:
+            recommended_movie_object = Movie(id,title,overview,poster)
+            recommended_movies_results.append(recommended_movie_object)
+
+    return recommended_movies_results
+
+def search_movie(movie_name):
+    search_movie_url = search_url.format(api_key,movie_name)
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        search_movie_response = json.loads(search_movie_data)
+
+        search_movie_results = None
+
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
+
+    return search_movie_results
--- config.py
+++ config.py
@@ -4,6 +4,9 @@ class Config:
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = 'e699c48f914c566cf9dccd32eac5fe7c'
     TRAILERS_URL = 'https://api.themoviedb.org/3/movie/{}/videos?api_key={}&language=en-US'
+    RECOMMENDED_MOVIES_URL = 'https://api.themoviedb.org/3/movie/{}/recommendations?api_key={}&language=en-US&page=1'
+    SIMILAR_MOVIES_URL = 'https://api.themoviedb.org/3/movie/{}/similar?api_key={}&language=en-US&page=1'
+    SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'
 
 class ProdConfig(Config):
 
