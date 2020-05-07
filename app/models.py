af7c8a68d0335836a7167f965c5e419537efcd97
derrickip34 <derrickip34@gmail.com>
2020-05-07 16:04:03 +0300

    create a request to get a single movie

--- app/models.py
+++ app/models.py
@@ -1,8 +1,7 @@
 class Movie:
 
-    def __init__(self,id,title,overview,poster,genre):
+    def __init__(self,id,title,overview,poster):
         self.id =id
         self.title = title
         self.overview = overview
         self.poster = "https://image.tmdb.org/t/p/w500/" + poster
-        self.genre = genre
--- app/request.py
+++ app/request.py
@@ -33,10 +33,27 @@ def process_results(movie_list):
         title = movie_item.get('original_title')
         overview = movie_item.get('overview')
         poster = movie_item.get('poster_path')
-        genre = movie_item,get('genre')
 
         if poster:
-            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+            movie_object = Movie(id,title,overview,poster)
             movie_results.append(movie_object)
 
     return movie_results
+
+def get_movie(id):
+    get_movie_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_movie_details_url) as url:
+        movie_details_data = url.read()
+        movie_details_response = json.loads(movie_details_data)
+
+        movie_object = None
+        if movie_details_response:
+            id = movie_details_response.get('id')
+            title = movie_details_response.get('original_title')
+            overview = movie_details_response.get('overview')
+            poster = movie_details_response.get('poster_path')
+
+            movie_object = Movie(id,title,overview,poster)
+
+    return movie_object
