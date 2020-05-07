0381eb1c3187ca9d98328e71e640c8928a96b473
derrickip34 <derrickip34@gmail.com>
2020-05-07 15:34:53 +0300

    add api key and base url

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    MOVIE_API_KEY = 'e699c48f914c566cf9dccd32eac5fe7c'
 
 class ProdConfig(Config):
 
