f4944c5bcffac58b256c03028e5f590b1c52e7b1
hukish <hudsonhukish@gmail.com>
2020-05-08 10:44:10 +0300

    added a login class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -20,3 +20,5 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+
+class LoginForm(FlaskForm):
